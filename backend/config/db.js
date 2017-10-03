aae37b166b2d7a3439499dd36c3e22fc81e86075
vinaysinghbaghel <vinaybaghel180@gmail.com>
2017-10-03 19:46:43 +0530

    full and final for deploy

--- backend/config/db.js
+++ backend/config/db.js
@@ -15,11 +15,22 @@ mongoose.connection.on('error', function(err) {
     console.log('Could not connect to mongo server!');
     console.log(err);
 });
-var dbURI = "mongodb://vinnu:vinnu@ds161584.mlab.com:61584/sugarapp";
+// var dbURI = "mongodb://vinnu:vinnu@ds161584.mlab.com:61584/sugarapp";
 // if (process.env.NODE_ENV === 'production') {
 //     dbURI = process.env.MONGOLAB_URI;
 // }
-mongoose.connect(dbURI, function(error) {
+var env = process.env.NODE_ENV = process.env.NODE_ENV || 'dev'
+var con=''
+if(env==='dev') {
+    con ='mongodb://localhost:27017/sugarapp'
+}
+else
+{
+    con='mongodb://vinnu:vinnu@ds161584.mlab.com:61584/sugarapp';
+}
+// console.log(con);
+// exports.connectionString= con;
+mongoose.connect(con, function(error) {
     console.log('error', error);
 });
 
@@ -28,3 +39,14 @@ module.exports = mongoose.connection;
 
 // }
 // mongoose.connect(dbURI);
+// var env = process.env.NODE_ENV = process.env.NODE_ENV || 'dev'
+// var con=''
+// if(env==='dev') {
+//     con ='mongodb://localhost:27017/test'
+// }
+// else
+// {
+//     con='mongodb://address:book@ds021711.mlab.com:21711/addressbook1234';
+// }
+// console.log(con);
+// exports.connectionString= con;
