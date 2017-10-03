1603a7d9fbcd58f51b471e1c8e45d05ea4752ce1
vinaysinghbaghel <vinaybaghel180@gmail.com>
2017-10-03 14:16:10 +0530

    add data

--- backend/config/config.js
+++ backend/config/config.js
@@ -9,7 +9,7 @@ var config = {};
 
  config.mongodb = {};
 // config.mongodb.url = 'mongodb://vinay singh:wishto@23@ds011374.mlab.com:11374/sugarapp';
- config.mongodb.url = 'mongodb://localhost:27017/sugarapp';
+ config.mongodb.url = 'mongodb://127.0.0.1:27017/sugarapp';
 
 
 
